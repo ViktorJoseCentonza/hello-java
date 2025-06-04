ea8ec1c672d92ff5af41e8e0551b36ca8d6279b7
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-06-04 11:00:00 +0200

    change component name as instructed

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -1,6 +1,6 @@
 package org.lessons.java;
 
-public class ClasseProva {
+public class StampaSaluto {
     public static void main(String[] args) {
         System.out.println("Io sono la classe di prova");
     }
